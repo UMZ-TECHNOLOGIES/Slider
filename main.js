35b0263288d92a157381f22f8bf73aa8390d0e09
Souvik Koley <souvikdk.2024@gmail.com>
2024-04-19 19:46:54 +0530

    Final Commit

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
-const menuBtn=document.querySelector(".menu-btn");
-const navigation = document.querySelector
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".img-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function (manuL) {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+  btns[manuL].classList.add("active");
+  slides[manuL].classList.add("active");
+  contents[manuL].classList.add("active");
+};
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    sliderNav(i);
+  });
+});
